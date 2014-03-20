d08cadeef15e7a24a16141a268be9420a04fd62a
Michael Norris <github@michaelnorris.co.uk>
2014-03-20 15:03:33 +0000

    refactored variable names

--- src/common/Global.java
+++ src/common/Global.java
@@ -4,20 +4,20 @@ package common;
  * Major constants used in the game
  */
 public class Global {
-    public static final int height = 450;          // Height of window
-    public static final int width = 600;          // Width  of window
+    public static final int windowHeight = 450;          // Height of window
+    public static final int windowWidth = 600;          // Width  of window
 
-    public static final double B = 6;            // Border offset
-    public static final double M = 26;           // Menu offset
-    public static final double ball_size = 15;  // Ball side
-    public static final double BAT_WIDTH = 10;  // Bat width
-    public static final double BAT_HEIGHT = 100; // Bat Height
+    public static final double borderOffset = 6;            // Border offset
+    public static final double menuOffset = 26;           // Menu offset
+    public static final double ballSize = 15;  // Ball side
+    public static final double batWidth = 10;  // Bat windowWidth
+    public static final double batHeight = 100; // Bat Height
 
-    public static final double BAT_MOVE = 5;       // Each move is
+    public static final double batMove = 5;       // Each move is
 
     // Of course this should not be a constant
     //  but should be user settable
     public static final int PORT = 50001;       // Port
-    public static final String HOST = "localhost"; // M/C Name
+    public static final String HOST = "localhost"; // menuOffset/C Name
 }
 
