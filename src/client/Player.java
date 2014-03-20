fbfdb4e93f11c971f7e9b26655ef8d1be3c557d4
Michael Norris <github@michaelnorris.co.uk>
2014-03-20 14:57:40 +0000

    change run method to read bat and ball positions

--- src/client/Player.java
+++ src/client/Player.java
@@ -51,9 +51,22 @@ class Player extends Thread {
 
         while(true) {
             // check to see if player has been initialised > 1
+            // this means a game has started and we've been
             if (playerNumber > -1) {
 
             }
+
+            String dataString = (String)playerNetObjectReader.get();
+            String[] dataArray = dataString.split(",");
+            assert (dataArray.length == 7); // assume that there are 7 values
+            playerNumber = Integer.parseInt(dataArray[0]); // get first element of array (player number)
+            int bat1x = Integer.parseInt(dataArray[1]);
+            int bat1y = Integer.parseInt(dataArray[2]);
+            int bat2x = Integer.parseInt(dataArray[3]);
+            int bat2y = Integer.parseInt(dataArray[4]);
+            int ballx = Integer.parseInt(dataArray[5]);
+            int bally = Integer.parseInt(dataArray[6]);
+
         }
     }
 }
