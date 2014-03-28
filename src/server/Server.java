c446a4ce14ba9fa1531471626508329074c52c63
Michael Norris <github@michaelnorris.co.uk>
2014-03-28 15:19:33 +0000

    move position of ArrayList to top of class. Describe the DEBUG output

--- src/server/Server.java
+++ src/server/Server.java
@@ -12,8 +12,12 @@ class Server {
     private NetObjectWriter player1NetObjectWriter,
             player2NetObjectWriter;
     private Player player1, player2;
+    /* ArrayList to hold the game objects */
+    ArrayList<Game> games = new ArrayList<Game>();
     /**
      * @param args
+     * When this method is run, call the start() method and create a
+     * new Server
      */
     public static void main(String args[]) {
         (new Server()).start();
@@ -22,20 +26,30 @@ class Server {
      * Start the server
      */
     public void start() {
+        /*
+        * We could remove the comment block surrounding the DEBUG lines
+        * below so to disable output to the console when the server
+        * starts. However it is good to keep it in to give feedback
+        * to the user who may be unsure if anything is happening as
+        * it will just wait until two Clients are run */
         DEBUG.set(true);
-        DEBUG.trace("Pong Server");
-        DEBUG.set(false); // Otherwise lots of debug info
-        ServerSocket s = null;
+        DEBUG.trace("Staring Pong Server...");
+        /* Here we stop the debug mode otherwise we will get the
+        * message 'ServerPongModel.modelChanged' printed to the console
+        * every 20 milliseconds */
+        DEBUG.set(false);
+        ServerSocket serverSocket = null;
         try {
-            s = new ServerSocket(Global.PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
+            serverSocket = new ServerSocket(Global.PORT);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-        assert (s != null);
-        ArrayList<Game> games = new ArrayList<Game>();
+         /* Will break if serverSocket doesn't exist */
+        assert (serverSocket != null);
         while (true) {
-            Game game = new Game(s);
+            Game game = new Game(serverSocket);
             game.start();
+            /* Add game to games array */
             games.add(game);
         }
     }
