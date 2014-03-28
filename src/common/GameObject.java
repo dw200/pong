98a4ab8143b9ee725d0fb47ec346a4f7fd7dd506
Michael Norris <github@michaelnorris.co.uk>
2014-03-28 14:37:48 +0000

    refactor variable names to be more meaningful

--- src/common/GameObject.java
+++ src/common/GameObject.java
@@ -2,76 +2,84 @@ package common;
 import java.io.*;
 /*
  * An Object in the game, represented as a rectangle
- *  Holds details of shape, plus possible direction of travel
+ * Holds details of shape, plus possible direction of travel
  */
 public class GameObject implements Serializable {
     private static final long serialVersionUID = 1L;
     public enum Collision {HIT, NO_HIT};
     // All the variables below are vital to the state of the object
-    private double topX = 0.0;          // Top left corner X
-    private double topY = 0.0;          // Top left corner Y
-    private double width = 0.0;         // Width of object
-    private double height = 0.0;        // Height of object
-    private double dirX = 1;            // Direction X (1 or -1)
-    private double dirY = 1;            // Direction Y (1 or -1)
+    /* Top left corner X */
+    private double gameObjectPositionX = 0.0;
+    /* Top left corner Y */
+    private double gameObjectPositionY = 0.0;
+    /* Width of object */
+    private double gameObjectWidth = 0.0;
+    /* Height of object */
+    private double gameObjectHeight = 0.0;
+    /* Direction X (1 or -1) */
+    private double directionX = 1;
+    /* Direction Y (1 or -1) */
+    private double directionY = 1;
     /**
      * Create an instance of a Game object
-     * @param x       Top left hand corner x
-     * @param y       Top left hand corner y
-     * @param aWidth  Width of object
-     * @param aHeight Height of Object
+     * @param gameObjectPositionX Top left hand corner x
+     * @param gameObjectPositionY Top left hand corner y
+     * @param gameObjectWidth  Width of object
+     * @param gameObjectHeight Height of Object
      */
-    public GameObject(double x, double y, double aWidth, double aHeight) {
-        topX = x;
-        topY = y;
-        width = aWidth;
-        height = aHeight;
+    public GameObject(double gameObjectPositionX,
+                      double gameObjectPositionY,
+                      double gameObjectWidth, double gameObjectHeight) {
+        this.gameObjectPositionX = gameObjectPositionX;
+        this.gameObjectPositionY = gameObjectPositionY;
+        this.gameObjectWidth = gameObjectWidth;
+        this.gameObjectHeight = gameObjectHeight;
     }
-    public double getX() {
-        return topX;
+    public double getGameObjectPositionX() {
+        return gameObjectPositionX;
     }
-    public double getY() {
-        return topY;
+    public double getGameObjectPositionY() {
+        return gameObjectPositionY;
     }
-    public double getWidth() {
-        return width;
+    public double getGameObjectWidth() {
+        return gameObjectWidth;
     }
-    public double getHeight() {
-        return height;
+    public double getGameObjectHeight() {
+        return gameObjectHeight;
     }
-    public void setX(double x) {
-        topX = x;
+    public void setGameObjectPositionX(double gameObjectPositionX) {
+        this.gameObjectPositionX = gameObjectPositionX;
     }
-    public void setY(double y) {
-        topY = y;
+    public void setGameObjectPositionY(double gameObjectPositionY) {
+        this.gameObjectPositionY = gameObjectPositionY;
     }
     /**
      * Move object by X units
      * The actual direction moved is flipped by changeDirectionX()
-     * @param units Units to move game object by X direction
+     * @param unitsToMove Units to move game object by X direction
      */
-    public void moveX(double units) {
-        topX += units * dirX;
+    public void moveX(double unitsToMove) {
+        gameObjectPositionX += unitsToMove * directionX;
     }
     /**
      * Move object by Y units
      * The actual direction moved is flipped by changeDirectionY()
-     * @param units Units to move game object by Y direction
+     * @param unitsToMove Units to move game object by Y direction
      */
-    public void moveY(double units) {
-        topY += units * dirY;
+    public void moveY(double unitsToMove) {
+        gameObjectPositionY += unitsToMove * directionY;
     }
     /**
      * Change direction of future moves in the X direction
      */
     public void changeDirectionX() {
-        dirX = -dirX;
+        directionX = -directionX;
     }
     /**
      * Change direction of future moves in the Y direction
      */
     public void changeDirectionY() {
-        dirY = -dirY;
+        directionY = -directionY;
     }
     /**
      * Detect a collision between two GameObjects
@@ -79,10 +87,14 @@ public class GameObject implements Serializable {
      * @param with Check if object (with) collides with us
      */
     public Collision collision(GameObject with) {
-        if (topX >= with.getX() + with.getWidth() ||
-                topX + width <= with.getX() ||
-                topY >= with.getY() + with.getHeight() ||
-                topY + height <= with.getY())
+        if (gameObjectPositionX >= with.getGameObjectPositionX() +
+                with.getGameObjectWidth() ||
+                gameObjectPositionX + gameObjectWidth <= with
+                        .getGameObjectPositionX() ||
+                gameObjectPositionY >= with.getGameObjectPositionY()
+                        + with.getGameObjectHeight() ||
+                gameObjectPositionY + gameObjectHeight <= with
+                        .getGameObjectPositionY())
             return Collision.NO_HIT;
         else {
             return Collision.HIT;
