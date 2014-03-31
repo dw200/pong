d1e0025f6776bbfd4c6824c0162816de48b3ece4
Michael Norris <github@michaelnorris.co.uk>
2014-03-31 14:09:07 +0100

    Remove empty lines.

--- src/client/ClientPongController.java
+++ src/client/ClientPongController.java
@@ -1,20 +1,15 @@
 package client;
-
 import common.GameObject;
 import common.Global;
-
 import java.awt.event.KeyEvent;
-
 /**
  * Pong controller, handles user interactions
  */
 public class ClientPongController {
     private final ClientPongModel model;
     private ClientPongView view;
-
     /**
      * Constructor
-     *
      * @param aPongModel Model of game on client
      * @param aPongView  View of game on client
      */
@@ -23,24 +18,18 @@ public class ClientPongController {
         view = aPongView;
         view.setPongController(this);  // View talks to controller
     }
-
     /**
      * Decide what to do for each key pressed
-     *
      * @param keyCode The keycode of the key pressed
      */
     public void userKeyInteraction(int keyCode) {
-
         if (model.getPlayerNumber() <= -1) {
             return;
         }
-
         // Key typed includes specials, -ve
         // Char is ASCII value
-
         synchronized (model) {
             GameObject bat = model.getBats()[model.getPlayerNumber()];
-
             switch (keyCode)              // Character is
             {
                 case -KeyEvent.VK_LEFT:        // Left Arrow
@@ -56,11 +45,7 @@ public class ClientPongController {
                     bat.setGameObjectPositionY(bat.getGameObjectPositionY() + Global.batMove);
                     break;
             }
-
             model.modelChanged();
         }
     }
-
-
 }
-
