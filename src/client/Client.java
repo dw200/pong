966c42f14ac2af0cf40f1b1401e8bcaec4f0d6b1
Michael Norris <github@michaelnorris.co.uk>
2014-04-01 16:40:51 +0100

    Remove redundant parameter 'controller' in makeContactWithServer

--- src/client/Client.java
+++ src/client/Client.java
@@ -15,14 +15,14 @@ class Client {
      */
     public void start() {
         DEBUG.set(true);
-        DEBUG.trace("Pong Client");
+        DEBUG.trace("Starting Pong Client...");
         DEBUG.set(false);
         /* pass -1 (player number) as no player yet */
         ClientPongModel model = new ClientPongModel(-1);
         ClientPongView view = new ClientPongView();
         ClientPongController cont = new ClientPongController(model,
                 view);
-        makeContactWithServer(model, cont);
+        makeContactWithServer(model);
         /* Add observer to the model */
         model.addObserver(view);
         /* Display Screen */
@@ -32,10 +32,8 @@ class Client {
      * Make contact with the Server who controls the game
      * Players will need to know about the model
      * @param model Of the game
-     * @param cont  Controller (MVC) of the Game
      */
-    public void makeContactWithServer(ClientPongModel model,
-                                      ClientPongController cont) {
+    public void makeContactWithServer(ClientPongModel model) {
         // Also starts the Player task that get the current state
         //  of the game from the server
         Socket serverSocket = null;
