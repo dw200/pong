13138593fa86ab7eca259b57fa1e7f4370ee8234
Michael Norris <github@michaelnorris.co.uk>
2014-04-02 11:49:39 +0100

    Refactored code and change comment style

--- src/server/ServerPongView.java
+++ src/server/ServerPongView.java
@@ -23,14 +23,22 @@ class ServerPongView implements Observer {
         ServerPongModel model = (ServerPongModel) aPongModel;
         ball = model.getBall();
         bats = model.getBats();
-        // Now need to send position of game objects to the client
-        // as the model on the server has changed
-        // Left off first parameter so we can reuse
+        /**
+         * Now need to send position of game objects to the client
+         * as the model on the server has changed
+         * Left off first parameter so we can reuse
+         */
         String dataString = String.format(",%f,%f,%f,%f,%f,%f",
-                bats[0].getGameObjectPositionX(), bats[0].getGameObjectPositionY(), bats[1].getGameObjectPositionX(),
-                bats[1].getGameObjectPositionY(), ball.getGameObjectPositionX(), ball.getGameObjectPositionY());
-        // reusing the dataString above and appending the player
-        // number to the beginning
+                bats[0].getGameObjectPositionX(),
+                bats[0].getGameObjectPositionY(),
+                bats[1].getGameObjectPositionX(),
+                bats[1].getGameObjectPositionY(),
+                ball.getGameObjectPositionX(),
+                ball.getGameObjectPositionY());
+        /**
+         * reusing the dataString above and appending the player
+         * number to the beginning
+         */
         client1.put("0" + dataString);
         client2.put("1" + dataString);
     }
